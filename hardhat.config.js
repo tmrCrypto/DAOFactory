8472676c9b762f132e55ff35b26f0fa844135d22
timur.musabekov <timur.musabekov@demlabs.net>
2025-03-04 15:54:20 +0700

    config_new_param

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    shannon: {
+      url: "https://testnet.somnia.network", 
+      accounts: [process.env.PRIVATE_KEY], 
+    },
+  },
+  etherscan: {
+    
+    apiKey: {
+      shannon: [process.env.API_KEY],
+    customChains: [
+      {
+        network: "shannon",
+        chainId: 50312, 
+        urls: {
+          apiURL: "https://shannon-explorer.somnia/api", 
+          browserURL: "https://shannon-explorer.somnia.network/", 
+        },
+      },
+    ],
+  },
 };
